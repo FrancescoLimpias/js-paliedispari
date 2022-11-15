cbed3a9043a0985e5f0ee3b3b7897770466980b6
Francesco Limpias <francescolimpias@gmail.com>
2022-11-15 16:26:31 +0100

    Milestone: dice roll

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function isPali(word) {
 
     // If word is undefined or empty
     // then it is considered not a palindrome
-    if(!word || word.trim().length == 0){
+    if (!word || word.trim().length == 0) {
         return false;
     }
 
@@ -26,4 +26,66 @@ function isPali(word) {
 
     // Word is pali
     return true;
+}
+
+// Play dice roll
+function playDice(userParity, userNumber) {
+
+    // Get userParity
+    if (!userParity) {
+        userParity = prompt("Pari o dispari?")
+    }
+
+    // Normalize userParity
+    userParity = userParity.toLowerCase();
+
+    // Check if userParity has an accepted value
+    if (userParity != "pari" && userParity != "dispari") {
+        // not an accepted value
+        alert("Puoi scegliere solo tra pari e dispari");
+
+        // try again user
+        return playDice(undefined, userNumber);
+
+    }
+
+    // Get userNumber
+    if (!userNumber) {
+        userNumber = parseInt(prompt("Scegli un numero da 1 a 5"));
+    }
+
+    // Check if userNumber has an accepted value
+    if (
+        isNaN(userNumber)
+        || userNumber < 1
+        || userNumber > 5
+    ) {
+        // userNumber is not valid
+        alert("Non hai inserito un valido numero");
+
+        // try again user
+        return playDice(userParity, undefined);
+    }
+
+    // Get computer throw
+    const computerNumber = rollDice();
+
+    // Result
+    const win = ((computerNumber + userNumber) % 2) == (userParity == "dispari");
+
+    // Print result in console
+    console.log(`Computer: ${computerNumber}`);
+    console.log(`Utente: ${userNumber}`);
+    if (win) {
+        console.log("Hai vinto!");
+    } else {
+        console.log("Hai perso");
+    }
+
+    return win;
+}
+
+// Roll a single dice
+function rollDice() {
+    return 1 + Math.round(Math.random() * 4);
 }
